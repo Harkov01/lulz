19fd4c4ef09a76472597f1cae60f2c1e884c4a80
Harkov01 <162648692+Harkov01@users.noreply.github.com>
2024-03-07 22:00:00 -0300

    Add files via upload

---
+++ js/openurl.js
@@ -0,0 +1,8 @@
+var steamLink = 'https://steamcommunity.com/id/harkov01/';
+var discordLink = 'https://discord.com/users/1215238555538821152';
+var spotifyLink = '/none';
+var instagramLink = '/none';
+
+function openURL(url) {
+    window.open(url, '_blank');
+}
