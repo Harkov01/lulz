4453facf515ce703b953a1fe63d3ec613a831e5e
Harkov01 <162648692+Harkov01@users.noreply.github.com>
2024-03-07 22:41:04 -0300

    Update openurl.js

--- js/openurl.js
+++ js/openurl.js
@@ -1,7 +1,7 @@
 var steamLink = 'https://steamcommunity.com/id/harkov01/';
 var discordLink = 'https://discord.com/users/1215238555538821152';
-var spotifyLink = '/none';
-var instagramLink = '/none';
+var spotifyLink = 'https://harkov01.github.io/lulz/';
+var instagramLink = 'https://harkov01.github.io/lulz/';
 
 function openURL(url) {
     window.open(url, '_blank');
